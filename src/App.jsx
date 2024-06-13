321d7ad8a95a58fa06a4d068316bcacbc5674fcd
V <voltdrake@gmail.com>
2024-06-13 15:32:41 -0400

    Configure React app for deployment to GitHub Pages

---
+++ src/App.jsx
@@ -0,0 +1,188 @@
+import { Link } from "react-router-dom";
+import {useState} from 'react'
+import json from './assets/extractedMainQuests.json'
+import {AutoSizer, List, CellMeasurer, CellMeasurerCache} from 'react-virtualized'
+
+function MainQuest({mainquest}){
+  return (
+    <div className = 'bg-gray-300 border-4 border-black rounded-3xl min-h-[350px] w-[calc(25%-10px)] m-[5px]'>
+      <Link className = 'block h-full' to={`/quest/${mainquest.questId}`}>
+        <p className = 'mt-1 font-bold text-center'>{mainquest.chapterNum}</p>
+        <p className = 'font-bold text-center'>{mainquest.chapterTitle}</p>
+        <p className = 'font-bold text-center'>{mainquest.questTitle}</p>
+        <p className = 'm-3'>{mainquest.questDesc}</p>
+      </Link>
+    </div>
+  )
+}
+
+function ColorText({text, speaker, color}){ //this is overtop of highlights as i can't code this seprately sorry
+  let textString
+  if (color.length > 0){
+    for (const attr of color){
+      textString = text.replaceAll(attr.text, '<span class="text-[' + attr.color + ']">' + attr.text + '</span>')
+    }
+  }
+  else textString = text
+  const comWord = '<span class="font-bold">' + speaker + '</span>' + ': ' + textString
+  return <p className = "absolute top-0 left-0 ml-5" dangerouslySetInnerHTML={{__html: comWord}}/>
+}
+
+function HighlightText({text, speaker, highlightTxt}){
+  const textString = text.replaceAll(highlightTxt, '<span class="bg-yellow-300">' + highlightTxt + '</span>')
+  const speakerString = speaker.replaceAll(highlightTxt, '<span class="bg-yellow-300">' + highlightTxt + '</span>')
+  const comWord = '<span class="font-bold">' + speakerString + '</span>' + ': ' + textString
+
+  return <p className = "ml-5" dangerouslySetInnerHTML={{__html: comWord}}/>
+}
+
+export default function App() {
+  const [searchedWordList, setSearchedWordList] = useState([])
+  const [filteredWord, setWord] = useState('')
+  const [curDim, setCurrentDim] = useState({height: window.innerHeight, width: window.innerWidth})
+
+  const cache = new CellMeasurerCache({
+    defaultHeight: 30,
+    fixedWidth: true
+  });
+
+  function searchSelectedWord(word){
+
+    if (word === '') return setSearchedWordList([]) //don't search empty strings, pls
+
+    // Regular expression to match <color=#COLOR_CODE>{TEXT}</color> pattern
+    const regex = /<color=#([0-9A-Fa-f]{8})>(.*?)<\/color>/g;
+
+    function removeColorTags(text) {
+    
+      // Replace matched patterns with just the text inside the tags
+      const result = text.replace(regex, (match, p1, p2) => {
+        // p2 is the captured text inside the <color> tags
+        return p2;
+      });
+    
+      return result;
+    }
+
+    function extractColorTags(text) {
+      // Regular expression to match <color=#COLOR_CODE>{TEXT}</color> pattern
+      const matches = [];
+      let match;
+      // Iterate over all matches
+      while ((match = regex.exec(text)) !== null) {
+        matches.push({
+          color: `#${match[1]}`, // Extracted color code
+          text: match[2]          // Extracted text inside the tags
+        });
+      }
+      return matches;
+    }
+    
+    function checkWordExist(word, speaker, dialog){
+      if (speaker.includes(word) || dialog.includes(word)) {
+        return true
+      }
+      return false
+    }
+
+    const matchingWords = []
+    for (const quest of json){
+      for (const subquest of quest.subQuestList){
+        for (const dialog of subquest.dialogList) {
+          if (dialog.dialogType === "Single") {
+            if (checkWordExist(word, dialog.speakerName, removeColorTags(dialog.dialogText)))
+              matchingWords.push({quest: `${quest.chapterNum}, ${quest.chapterTitle}: ${quest.questTitle}`, 
+                speaker: dialog.speakerName, 
+                questId: quest.questId,
+                color: extractColorTags(dialog.dialogText),
+                text: removeColorTags(dialog.dialogText)})
+          } 
+          else {
+            for (const multi of dialog.multiDialogList) {
+              if (checkWordExist(word, multi.speakerName, removeColorTags(multi.dialogText))) 
+                matchingWords.push({quest: `${quest.chapterNum}, ${quest.chapterTitle}: ${quest.questTitle}`, 
+                  speaker: multi.speakerName, 
+                  questId: quest.questId,
+                  color: extractColorTags(multi.dialogText),
+                  text: removeColorTags(multi.dialogText)})
+            }
+          }
+        }
+      }
+    }
+    if (matchingWords.length === 0) return setSearchedWordList([null])
+
+    setSearchedWordList(matchingWords)
+  }
+
+  function handleInputChange(event){
+    setWord(event.target.value)
+    searchSelectedWord(event.target.value)
+  }
+
+  return (
+    <div className='font-serif flex flex-col h-screen'>
+      <div>
+        <h1 className='text-3xl font-bold'>Genshin Dialog Viewer</h1>
+        <label htmlFor = 'searchBox'>Search: </label>
+        <input 
+          id = 'searchBox' placeholder='Type to search' value = {filteredWord}
+          className = 'bg-gray-100 border border-black rounded pl-1 inline-flex' 
+          onChange={e => handleInputChange(e)}
+          type = 'search'>
+        </input>
+      </div>
+      {searchedWordList.length > 0?
+        <div className="h-full">
+          <AutoSizer
+            onResize={({width, height}) => setCurrentDim({width: width, height: height})}
+          >
+            {() => (
+              <List
+                height={curDim.height}
+                width={curDim.width}
+                deferredMeasurementCache={cache}
+                rowHeight={cache.rowHeight}
+                rowCount={searchedWordList.length}
+                rowRenderer={({ index, key, style, parent}) => (
+                  <CellMeasurer 
+                    key={key} 
+                    cache={cache} 
+                    columnIndex={0} 
+                    parent={parent} 
+                    rowIndex={index}>
+                      { searchedWordList[0] === null?
+                        <p className = 'm-1' style={style}>No Matched Words Found!</p>
+                        :
+                        <div style = {style}>
+                          <div className="border-2 border-black m-1 p-4 rounded-xl bg-gray-300">
+                            <Link to={`/quest/${(searchedWordList[index]).questId}`}><p className='font-bold'>{(searchedWordList[index]).quest}</p></Link>
+                            <div className="relative">
+                              <HighlightText speaker = {(searchedWordList[index]).speaker} 
+                                text = {(searchedWordList[index]).text} 
+                                highlightTxt = {filteredWord}/>
+                              <ColorText speaker = {(searchedWordList[index]).speaker} 
+                                text = {(searchedWordList[index]).text} 
+                                color = {(searchedWordList[index]).color}/>
+                            </div>
+                          </div>
+                        </div>
+                      }
+                  </CellMeasurer>
+                )}
+              />
+            )}
+          </AutoSizer>
+        </div>
+        :
+        <div className='flex flex-wrap'>
+          {
+            json.map((mainquest)=>(
+              <MainQuest key = {mainquest.questId} mainquest = {mainquest}/>
+            ))
+          }
+        </div>
+      }
+    </div>
+  )
+}
---
+++ src/QuestText.jsx
@@ -0,0 +1,86 @@
+//import { useState } from 'react'
+import { useParams } from "react-router-dom";
+import json from './assets/extractedMainQuests.json'
+
+export default function QuestText() {
+  const {id} = useParams();
+  const quest = json.find((q) => q.questId === parseInt(id)) //find i
+  const regex = /<color=#([0-9A-Fa-f]{8})>(.*?)<\/color>/g;
+
+  function removeColorTags(text) {
+    
+    // Replace matched patterns with just the text inside the tags
+    const result = text.replace(regex, (match, p1, p2) => {
+      // p2 is the captured text inside the <color> tags
+      return p2;
+    });
+  
+    return result;
+  }
+
+  function extractColorTags(text) {
+    // Regular expression to match <color=#COLOR_CODE>{TEXT}</color> pattern
+    const matches = [];
+    let match;
+    // Iterate over all matches
+    while ((match = regex.exec(text)) !== null) {
+      matches.push({
+        color: `#${match[1]}`, // Extracted color code
+        text: match[2]          // Extracted text inside the tags
+      });
+    }
+    return matches;
+  }
+
+  function renderOutput(){
+    const sOut = [];
+    if (!('questType' in quest)) {
+      sOut.push(`<span class="font-bold">ID: ${quest.questId}, ?????</span>`);
+      sOut.push(`<span>---${quest.chapterNum}, ${quest.chapterTitle}, ${quest.questTitle}---</span>`);
+      return sOut;
+    }
+    sOut.push(`<span class="font-bold">ID: ${quest.questId}, ${quest.questType}</span>`);
+    sOut.push(`<span class="font-bold">---${quest.chapterNum}, ${quest.chapterTitle}, ${quest.questTitle}---</span>`);
+    sOut.push(`<span class="font-bold">Desc:</span> ${quest.questDesc}`);
+
+    for (const subquest of quest.subQuestList) {
+      sOut.push(`<span class="font-bold">---Quest: ${subquest.subQuestTitle}---</span>`);
+
+      for (const dialog of subquest.dialogList) {
+        if (dialog.dialogType === "Single") {
+          const color = extractColorTags(dialog.dialogText)
+          let textString = removeColorTags(dialog.dialogText)
+          if (color.length > 0){
+            for (const attr of color){ //#00E1FFFF
+              textString = textString.replaceAll(attr.text, '<span class="text-[' + attr.color + ']">' + attr.text + '</span>')
+            }
+          }
+          sOut.push(`<span class="font-bold">${dialog.speakerName}:</span> ${textString}`);
+        } 
+        else {
+          sOut.push('<span class="font-bold">---MultiDialog Start---</span>');
+          for (const multi of dialog.multiDialogList) {
+            const color = extractColorTags(multi.dialogText)
+            let textString = removeColorTags(multi.dialogText)
+            if (color.length > 0){
+              for (const attr of color){
+                textString = textString.replaceAll(attr.text, '<span class="text-[' + attr.color + ']">' + attr.text + '</span>')
+              }
+            }
+            sOut.push(`<span class="font-bold">${multi.speakerName}:</span> ${textString}`);
+          }
+          sOut.push('<span class="font-bold">---MultiDialog End---</span>');
+        }
+      }
+    }
+    return sOut
+  };
+
+  return (
+    <div className='font-serif'>
+      {renderOutput().map((line, index) => (
+        <p className = 'm-1 p-0.5' key={index} dangerouslySetInnerHTML={{__html: line}}/>
+      ))}
+    </div>
+  )
+}
